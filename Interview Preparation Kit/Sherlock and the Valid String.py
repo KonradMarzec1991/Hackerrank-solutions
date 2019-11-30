7830aabd09a665a983ec82643389a703876c5903
KonradMarzec1991 <konrimarzec@gmail.com>
2019-11-30 20:27:02 +0100

    Next solution

---
+++ Interview Preparation Kit/Sherlock and the Valid String.py
@@ -0,0 +1,16 @@
+import collections
+
+
+def isValid(s):
+    letters = dict(collections.Counter(s))
+    it_list = list(letters.values())
+    it_list.sort()
+
+    if it_list.count(it_list[0]) == len(it_list):
+        return 'YES'
+    elif it_list.count(it_list[0]) == len(it_list) - 1 and it_list[-1] - it_list[-2] == 1:
+        return 'YES'
+    elif it_list.count(it_list[-1]) == len(it_list) - 1 and it_list[0] == 1:
+        return 'YES'
+    else:
+        return 'NO'
