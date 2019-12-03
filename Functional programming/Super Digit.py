5dd46e4573922aff40561591f7746008a63ac10a
KonradMarzec1991 <konrimarzec@gmail.com>
2019-12-03 19:23:18 +0100

    Next hackerrank solutions

---
+++ Functional programming/Super Digit.py
@@ -0,0 +1,11 @@
+
+
+def superDigit(n, k):
+    final = sum(list(map(int, str(n))))*k
+
+    if final < 10:
+        return final
+
+    while int(final) > 10:
+        final = sum([int(v) for v in str(final)])
+    return int(final)
