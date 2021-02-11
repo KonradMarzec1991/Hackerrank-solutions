32b6316c79520374081e3e4010efb3aa8dfda546
KonradMarzec1991 <konrimarzec@gmail.com>
2021-02-11 22:01:10 +0100

    Add .gitignore

--- Functional programming/Super Digit.py
+++ Functional programming/Super Digit.py
@@ -1,11 +1,7 @@
-
-
 def superDigit(n, k):
     final = sum(list(map(int, str(n))))*k
-
     if final < 10:
         return final
-
     while int(final) > 10:
         final = sum([int(v) for v in str(final)])
     return int(final)
